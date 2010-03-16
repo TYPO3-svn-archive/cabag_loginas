55e7b70e90d682729b7ce61851d8d33b4fbe78b4
dk-at-cabag <dk-at-cabag@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-16 15:46:31 +0000

    updated to 0.0.6, see changelog
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cabag_loginas/trunk@31156 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "cabag_loginas".
 #
-# Auto generated 15-02-2010 15:25
+# Auto generated 16-03-2010 16:39
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.4',
+	'version' => '0.0.6',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"dae9";s:10:"README.txt";s:4:"d238";s:25:"class.tx_cabagloginas.php";s:4:"d8a9";s:41:"class.tx_cabagloginas_makecontrolhook.php";s:4:"02ac";s:12:"ext_icon.gif";s:4:"778f";s:17:"ext_localconf.php";s:4:"a16f";s:14:"ext_tables.php";s:4:"ce42";s:16:"locallang_db.xml";s:4:"a00d";s:19:"doc/wizard_form.dat";s:4:"b11c";s:20:"doc/wizard_form.html";s:4:"1f2f";s:33:"sv1/class.tx_cabagloginas_sv1.php";s:4:"3894";}',
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"e31b";s:10:"README.txt";s:4:"c532";s:17:"cabag_loginas.css";s:4:"9d74";s:24:"cabagloginas_toolbar.php";s:4:"59cb";s:25:"class.tx_cabagloginas.php";s:4:"3ef7";s:41:"class.tx_cabagloginas_makecontrolhook.php";s:4:"02ac";s:12:"ext_icon.gif";s:4:"778f";s:17:"ext_localconf.php";s:4:"a16f";s:14:"ext_tables.php";s:4:"85f6";s:16:"locallang_db.xml";s:4:"a00d";s:19:"doc/wizard_form.dat";s:4:"b11c";s:20:"doc/wizard_form.html";s:4:"1f2f";s:33:"sv1/class.tx_cabagloginas_sv1.php";s:4:"92e1";}',
 	'suggests' => array(
 	),
 );
